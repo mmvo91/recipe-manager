d2753135404407eaa8de481c753fcd45f9085a60
Michael Vo <michaelmvo91@gmail.com>
2020-10-30 22:52:57 -1000

    Adding Imports feature routes

---
+++ backend/api/features/imports/routes.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+
+from fastapi import Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from api.utils.db import get_db
+from . import schemas, services
+
+imports_router = APIRouter()
+
+
+@imports_router.get("/", response_model=List[schemas.RecipeImport])
+def get_recipe_imports(
+    imported: Optional[bool] = None,
+    db: Session = Depends(get_db),
+):
+    return services.get_imported_recipes(db, imported=imported)
+
+
+@imports_router.get("/{imported_id}", response_model=schemas.SingleRecipeImport)
+def get_recipe_import(
+    imported_id: int,
+    db: Session = Depends(get_db),
+):
+    return services.get_imported_recipe(imported_id, db)
+
+
+@imports_router.put("/{imported_id}", response_model=schemas.SingleRecipeImport)
+def update_recipe_import(
+    imported_id: int,
+    db: Session = Depends(get_db),
+):
+    return services.update_imported_recipe(imported_id, db)
