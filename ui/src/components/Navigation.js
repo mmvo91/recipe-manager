16268cbb88a0b6c1f412b6358f6f577603089292
Michael Vo <michaelmvo91@gmail.com>
2020-11-08 16:03:47 -1000

    Implementing cookies in existing components

--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from "react-bootstrap/NavDropdown";
 import {LinkContainer} from 'react-router-bootstrap'
+import {useCookies} from "react-cookie";
 
 const NavLink = (props) => {
     return (
@@ -21,22 +22,39 @@ const NavDropDownItemLink = (props) => {
 };
 
 const Navigation = () => {
+    const [cookie, setCookie, removeCookie] = useCookies(['access_token'])
+
+    const loggingOut = () => {
+        removeCookie('access_token')
+    }
+
     return (
         <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark" className="sticky-top">
             <Navbar.Brand href="/">
                 {'Recipe App'}
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse className="text-center" id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <NavLink href={"/"}>Home</NavLink>
-                    <NavDropdown title="Recipes" id="collapsible-nav-dropdown">
-                        <NavDropDownItemLink href="/recipes">Recipes</NavDropDownItemLink>
-                        <NavDropDownItemLink href="/recipes/new">New</NavDropDownItemLink>
-                        <NavDropDownItemLink href="/import">Import</NavDropDownItemLink>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
+            {
+                cookie.access_token
+                    ? (
+                        <React.Fragment>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                            <Navbar.Collapse className="text-center" id="responsive-navbar-nav">
+                                <Nav className="mr-auto">
+                                    <NavLink href={"/"}>Home</NavLink>
+                                    <NavDropdown title="Recipes" id="collapsible-nav-dropdown">
+                                        <NavDropDownItemLink href="/recipes">Recipes</NavDropDownItemLink>
+                                        <NavDropDownItemLink href="/recipes/new">New</NavDropDownItemLink>
+                                        <NavDropDownItemLink href="/import">Import</NavDropDownItemLink>
+                                    </NavDropdown>
+                                </Nav>
+                                <Nav>
+                                    <Nav.Link href="/" onClick={loggingOut}>Logout</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </React.Fragment>
+                    )
+                    : null
+            }
         </Navbar>
     )
 };
