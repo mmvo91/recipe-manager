7773fa36cfe45ad4169c863c03e5c9766d927a18
Michael Vo <michaelmvo91@gmail.com>
2020-11-11 21:29:11 -1000

    Adding planner_router to app

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.features.imports.routes import imports_router
+from api.features.planner.routes import planner_router
 from api.features.recipes.routes import recipes_router
 from api.features.tokens.routes import token_router
 from api.features.users.routes import users_router
@@ -17,6 +18,7 @@ app.add_middleware(
 )
 
 app.include_router(imports_router, prefix="/api/imports")
+app.include_router(planner_router, prefix="/api/planner")
 app.include_router(recipes_router, prefix="/api/recipes")
 app.include_router(token_router, prefix="/api")
 app.include_router(users_router, prefix="/api/users")
